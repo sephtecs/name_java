27454108c5ca3aa5b878c71668b36023132aa647
Joseph <josephtecson18@gmail.com>
2021-09-28 15:50:15 -0700

    add files

---
+++ Name.java
@@ -0,0 +1,9 @@
+public class Name {
+    public static void main(String[] args) { 
+        //Every Java program will have one and only one class file that has a main method
+        //Typically, lines 1 & 2 will always remain the same
+        System.out.println("My name is Joseph"); //Semi-colon manadatory
+        System.out.println("I ame 30 years old");
+        System.out.println("My hometown is Glendale, CA");
+    }
+}
